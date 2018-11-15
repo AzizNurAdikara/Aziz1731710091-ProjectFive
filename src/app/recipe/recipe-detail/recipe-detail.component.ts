ffd5f709adcb5a01c40b597ca732f637c54a56df
Aziz Nur Adikara <37570196+AzizNurAdikara@users.noreply.github.com>
2018-11-15 18:05:47 +0700

    first commit

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService }from '../recipe.service';
-import { ActivatedRoute,Params } from '@angular/router';
+import { ActivatedRoute,Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +14,7 @@ recipe:Recipe;
 id:number;
 
   constructor(private recipeService:RecipeService,
-              private route:ActivatedRoute) { }
+              private route:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
     this.route.params
@@ -28,6 +28,9 @@ id:number;
 
   onAddShoppingList(){
     this.recipeService.addIngredientsShoppingList(this.recipe.ingredient);
+  }
+  onEditRecipe(){
+    this.router.navigate(['edit'],{relativeTo:this.route})
   }
 
 }
